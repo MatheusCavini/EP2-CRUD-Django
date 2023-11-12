2701d875f1f004f649637b5e27ac1b5c32f662cd
Matheus <matheuslcavini@usp.br>
2023-11-11 22:49:07 -0300

    Implement CRUD (create and update) with functional views and Django forms

--- posts/forms.py
+++ posts/forms.py
@@ -2,6 +2,30 @@ from django.forms import ModelForm
 from .models import Post, Comment
 from django import forms
 
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'text1',
+            'thumb',
+            'text2',
+            'img2'
+        ]
+        labels = {
+            'title':"Título",
+            'text1':"Primeiro bloco de texto:",
+            'thumb':"Link da primeira imagem:",
+            'text2':"Segundo bloco de texto:",
+            'img2':"Link da segunda imagem:",
+        }
+
+        widgets = {
+            'text1': forms.Textarea(attrs={'rows': 4, 'cols': 50}), 
+            'text2': forms.Textarea(attrs={'rows': 4, 'cols': 50}) 
+            
+        }
+
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .forms import CommentForm
+from .forms import CommentForm, PostForm
 
 from datetime import datetime
 
@@ -19,24 +19,28 @@ def detail_post(request, post_id):
 
 def create_post(request):
     if request.method == 'POST':
-        post_title = request.POST['name']
-        post_date = datetime.now()
-        post_thumb = request.POST['first_img']
-        post_image = request.POST['second_img']
-        post_text1 = request.POST['first_text']
-        post_text2 = request.POST['second_text']
-        post = Post(title=post_title,
-                      date=post_date,
-                      thumb=post_thumb,
-                      content= '<section class="ProjectContent"><p class="ProjectText">' + post_text1 + '</p><img class="ProjectImage" src="'+post_thumb+'" title="source: imgur.com" /><p class="ProjectText">' + post_text2+'</p><img  class="ProjectImage" src="'+post_image+'" title="source: imgur.com" /></section>',
-                      text1 = post_text1,
-                      text2 = post_text2,
-                      img2 = post_image)
-        post.save()
-        return HttpResponseRedirect(
-            reverse('posts:detail', args=(post.id, )))
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post_title = form.cleaned_data['title']
+            post_date = datetime.now()
+            post_thumb = form.cleaned_data['thumb']
+            post_image = form.cleaned_data['img2']
+            post_text1 = form.cleaned_data['text1']
+            post_text2 = form.cleaned_data['text2']
+            post = Post(title=post_title,
+                        date=post_date,
+                        thumb=post_thumb,
+                        content= '<section class="ProjectContent"><p class="ProjectText">' + post_text1 + '</p><img class="ProjectImage" src="'+post_thumb+'" title="source: imgur.com" /><p class="ProjectText">' + post_text2+'</p><img  class="ProjectImage" src="'+post_image+'" title="source: imgur.com" /></section>',
+                        text1 = post_text1,
+                        text2 = post_text2,
+                        img2 = post_image)
+            post.save()
+            return HttpResponseRedirect(
+                reverse('posts:detail', args=(post.id, )))
     else:
-        return render(request, 'posts/create.html', {})
+        form = PostForm()
+        context = {'form': form}
+        return render(request, 'posts/create.html', context)
 
 def delete_post(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
@@ -52,25 +56,35 @@ def update_post(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
 
     if request.method == "POST":
-        post_title = request.POST['name']
-        post_date = post.date
-        post_thumb = request.POST['first_img']
-        post_image = request.POST['second_img']
-        post_text1 = request.POST['first_text']
-        post_text2 = request.POST['second_text']
-        post.title =post_title
-        post.date =post_date
-        post.thumb =post_thumb
-        post.content = '<section class="ProjectContent"><p class="ProjectText">' + post_text1 + '</p><img class="ProjectImage" src="'+post_thumb+'" title="source: imgur.com" /><p class="ProjectText">' + post_text2+'</p><img  class="ProjectImage" src="'+post_image+'" title="source: imgur.com" /></section>'
-        post.text1 = post_text1
-        post.text2 = post_text2
-        post.img2 = post_image            
-        post.save()
-        return HttpResponseRedirect(
-            reverse('posts:detail', args=(post.id, )))
-
-    context = {'post': post}
-    return render(request, 'posts/update.html', context)
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post_title = form.cleaned_data['title']
+            post_date = datetime.now()
+            post_thumb = form.cleaned_data['thumb']
+            post_image = form.cleaned_data['img2']
+            post_text1 = form.cleaned_data['text1']
+            post_text2 = form.cleaned_data['text2']
+            post.title =post_title
+            post.date =post_date
+            post.thumb =post_thumb
+            post.content = '<section class="ProjectContent"><p class="ProjectText">' + post_text1 + '</p><img class="ProjectImage" src="'+post_thumb+'" title="source: imgur.com" /><p class="ProjectText">' + post_text2+'</p><img  class="ProjectImage" src="'+post_image+'" title="source: imgur.com" /></section>'
+            post.text1 = post_text1
+            post.text2 = post_text2
+            post.img2 = post_image            
+            post.save()
+            return HttpResponseRedirect(
+                reverse('posts:detail', args=(post.id, )))
+    else:
+        form = PostForm(
+            initial={
+                'title': post.title,
+                'text1': post.text1,
+                'text2': post.text2,
+                'thumb': post.thumb,
+                'img2': post.img2
+            })
+        context = {'post': post, 'form': form}
+        return render(request, 'posts/update.html', context)
 
 def create_comment(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
