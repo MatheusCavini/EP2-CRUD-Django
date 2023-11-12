ef8ac530de22fd43e5ef0c9e2a82ff700534b279
Matheus <matheuslcavini@usp.br>
2023-11-12 00:05:06 -0300

    Implement CRUD (create and update) with GenericViews and Django Forms

--- posts/urls.py
+++ posts/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-     path('', views.list_posts, name='index'),
-     path('<int:post_id>/', views.detail_post, name='detail'),
-     path('create/', views.create_post, name='create'),
-     path('delete/<int:post_id>/', views.delete_post, name='delete'),
-     path('update/<int:post_id>/', views.update_post, name='update'),
+     path('', views.PostListView.as_view(), name='index'),
+     path('<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+     path('create/', views.PostCreateView.as_view(), name='create'),
+     path('delete/<int:pk>/', views.DeletePostView.as_view(), name='delete'),
+     path('update/<int:pk>/', views.PostUpdateView.as_view(), name='update'),
      path('<int:post_id>/comment/', views.create_comment, name='comment'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,90 +1,75 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from .forms import CommentForm, PostForm
-
+from django.views import generic
 from datetime import datetime
 
 
-def list_posts(request):
-    posts_list = Post.objects.all()
-    context = {'posts_list': posts_list}
-    return render(request, 'posts/index.html', context)
 
-def detail_post(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    context = {'post': post}
-    return render(request, 'posts/detail.html', context)
 
-def create_post(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post_title = form.cleaned_data['title']
-            post_date = datetime.now()
-            post_thumb = form.cleaned_data['thumb']
-            post_image = form.cleaned_data['img2']
-            post_text1 = form.cleaned_data['text1']
-            post_text2 = form.cleaned_data['text2']
-            post = Post(title=post_title,
-                        date=post_date,
-                        thumb=post_thumb,
-                        content= '<section class="ProjectContent"><p class="ProjectText">' + post_text1 + '</p><img class="ProjectImage" src="'+post_thumb+'" title="source: imgur.com" /><p class="ProjectText">' + post_text2+'</p><img  class="ProjectImage" src="'+post_image+'" title="source: imgur.com" /></section>',
-                        text1 = post_text1,
-                        text2 = post_text2,
-                        img2 = post_image)
-            post.save()
-            return HttpResponseRedirect(
+class PostListView(generic.ListView):
+    model = Post
+    template_name = 'posts/index.html'
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    template_name = 'posts/detail.html'
+    
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/create.html'
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.date = datetime.now()
+        post_thumb = form.cleaned_data['thumb']
+        post_image = form.cleaned_data['img2']
+        post_text1 = form.cleaned_data['text1']
+        post_text2 = form.cleaned_data['text2']
+        post.content = f'<section class="ProjectContent"><p class="ProjectText">{post_text1}</p><img class="ProjectImage" src="{post_thumb}" title="source: imgur.com" /><p class="ProjectText">{post_text2}</p><img  class="ProjectImage" src="{post_image}" title="source: imgur.com" /></section>'
+        post.text1 = post_text1
+        post.text2 = post_text2
+        post.img2 = post_image
+        post.save()
+
+        return HttpResponseRedirect(reverse('posts:detail', args=(post.id,)))
+
+class DeletePostView(generic.DeleteView):
+    model = Post
+    template_name = 'posts/delete.html'
+    success_url = reverse_lazy("posts:index")
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/update.html'
+    success_url = reverse_lazy('posts:detail')
+
+    def form_valid(self, form):
+        post = form.instance
+        post_title = form.cleaned_data['title']
+        post_thumb = form.cleaned_data['thumb']
+        post_image = form.cleaned_data['img2']
+        post_text1 = form.cleaned_data['text1']
+        post_text2 = form.cleaned_data['text2']
+        post.title = post_title
+        post.date = datetime.now()
+        post.thumb = post_thumb
+        post.content = '<section class="ProjectContent"><p class="ProjectText">' + post_text1 + '</p><img class="ProjectImage" src="'+post_thumb+'" title="source: imgur.com" /><p class="ProjectText">' + post_text2 + '</p><img class="ProjectImage" src="'+post_image+'" title="source: imgur.com" /></section>'
+        post.text1 = post_text1
+        post.text2 = post_text2
+        post.img2 = post_image
+        post.save()
+        return HttpResponseRedirect(
                 reverse('posts:detail', args=(post.id, )))
-    else:
-        form = PostForm()
-        context = {'form': form}
-        return render(request, 'posts/create.html', context)
 
-def delete_post(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-
-    if request.method == "POST":
-        post.delete()
-        return HttpResponseRedirect(reverse('posts:index'))
-
-    context = {'post': post}
-    return render(request, 'posts/delete.html', context)
-
-def update_post(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post_title = form.cleaned_data['title']
-            post_date = datetime.now()
-            post_thumb = form.cleaned_data['thumb']
-            post_image = form.cleaned_data['img2']
-            post_text1 = form.cleaned_data['text1']
-            post_text2 = form.cleaned_data['text2']
-            post.title =post_title
-            post.date =post_date
-            post.thumb =post_thumb
-            post.content = '<section class="ProjectContent"><p class="ProjectText">' + post_text1 + '</p><img class="ProjectImage" src="'+post_thumb+'" title="source: imgur.com" /><p class="ProjectText">' + post_text2+'</p><img  class="ProjectImage" src="'+post_image+'" title="source: imgur.com" /></section>'
-            post.text1 = post_text1
-            post.text2 = post_text2
-            post.img2 = post_image            
-            post.save()
-            return HttpResponseRedirect(
-                reverse('posts:detail', args=(post.id, )))
-    else:
-        form = PostForm(
-            initial={
-                'title': post.title,
-                'text1': post.text1,
-                'text2': post.text2,
-                'thumb': post.thumb,
-                'img2': post.img2
-            })
-        context = {'post': post, 'form': form}
-        return render(request, 'posts/update.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post'] = self.object
+        return context
 
 def create_comment(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
